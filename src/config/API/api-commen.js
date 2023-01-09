6ede679f082eb42e3105a88e0f6374f379677c4e
Parth5096 <78688379+Parth5096@users.noreply.github.com>
2023-01-09 16:56:50 +0530

    Update api-commen.js

--- src/config/API/api-commen.js
+++ src/config/API/api-commen.js
@@ -1,5 +1,5 @@
 export const protocol = "http";
-export const host = "localhost";
+export const host = "31.220.57.38";
 // export const host = "10.10.30.180";
 export const port = "8080";
 export const trailUrl = "";
